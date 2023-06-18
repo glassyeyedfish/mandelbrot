a4e2ef608a771464a62eee0e09241b31ff7e5a49
glassyeyedfish <glassyeyedfish@gmail.com>
2023-06-18 11:01:39 -0400

    Added comments, cleanup

--- src/mandelbrot.c
+++ src/mandelbrot.c
@@ -1,26 +1,16 @@
-/*******************************************************************************************
-*
-*   raylib [core] example - Basic window (adapted for HTML5 platform)
-*
-*   This example is prepared to compile for PLATFORM_WEB, PLATFORM_DESKTOP and PLATFORM_RPI
-*   As you will notice, code structure is slightly diferent to the other examples...
-*   To compile it for PLATFORM_WEB just uncomment #define PLATFORM_WEB at beginning
-*
-*   This example has been created using raylib 1.3 (www.raylib.com)
-*   raylib is licensed under an unmodified zlib/libpng license (View raylib.h for details)
-*
-*   Copyright (c) 2015 Ramon Santamaria (@raysan5)
-*
-********************************************************************************************/
+/*
+ * mandelbrot.c
+ *
+ * Uses an algorithm adapted from https://en.wikipedia.org/wiki/Plotting_algorithms_for_the_Mandelbrot_set
+ */
 
 #include <raylib.h>
-
-#include "raymath.h"
 
 #if defined(PLATFORM_WEB)
     #include <emscripten/emscripten.h>
 #endif
 
+// A smooth gradient of blue to purple and back
 #define COLOR_00 (Color) {   4,   3,  48, 255 }
 #define COLOR_01 (Color) {  28,   8, 145, 255 }
 #define COLOR_02 (Color) {  74,  13, 242, 255 }
@@ -38,9 +28,6 @@
 #define COLOR_14 (Color) {  20,  13, 242, 255 }
 #define COLOR_15 (Color) {   8,  19, 145, 255 }
 
-//----------------------------------------------------------------------------------
-// Global Variables Definition
-//----------------------------------------------------------------------------------
 int screen_width = 1920;
 int screen_height = 1080;
 
@@ -70,22 +57,26 @@ Color colors[] = {
 Image buffer;
 Texture tex;
 
-//----------------------------------------------------------------------------------
-// Module Functions Declaration
-//----------------------------------------------------------------------------------
-void main_loop(void);     // Update and Draw one frame
-
-//----------------------------------------------------------------------------------
-// Main Enry Point
-//----------------------------------------------------------------------------------
-int main()
+/*
+ * The main loop simply draws the texture containing the visualization
+ */
+void 
+main_loop(void)
 {
-    // Initialization
-    //--------------------------------------------------------------------------------------
+    BeginDrawing();
+    DrawTexture(tex, 0, 0, WHITE);
+    EndDrawing();
+}
+
+int 
+main()
+{
+    // Setup
     InitWindow(screen_width, screen_height, "Mandelbrot Set Visualizer");
 
     buffer = GenImageColor(screen_width, screen_height, BLACK);
 
+    // The algorithm to generation the mandelbrot set visualization
     for (int x = 0; x < screen_width; x++) {
         for (int y = 0; y < screen_height; y++) {
             float x0 = (float) (x - (screen_width / 2) - x_offset) / zoom_factor;
@@ -114,44 +105,19 @@ int main()
 
     tex = LoadTextureFromImage(buffer);
 
+    // Two ways to call the main loop depending on the platform.
 #if defined(PLATFORM_WEB)
     emscripten_set_main_loop(main_loop, 0, 1);
 #else
-    SetTargetFPS(60);   // Set our game to run at 60 frames-per-second
-    //--------------------------------------------------------------------------------------
+    SetTargetFPS(60);
 
-    // Main game loop
-    while (!WindowShouldClose())    // Detect window close button or ESC key
+    while (!WindowShouldClose())
     {
         main_loop();
     }
 #endif
 
-    // De-Initialization
-    //--------------------------------------------------------------------------------------
-    CloseWindow();        // Close window and OpenGL context
-    //--------------------------------------------------------------------------------------
-
+    // Teardown
+    CloseWindow();
     return 0;
-}
-
-//----------------------------------------------------------------------------------
-// Module Functions Definition
-//----------------------------------------------------------------------------------
-void main_loop(void)
-{
-    // Update
-    //----------------------------------------------------------------------------------
-    // TODO: Update your variables here
-    //----------------------------------------------------------------------------------
-
-    // Draw
-    //----------------------------------------------------------------------------------
-    BeginDrawing();
-
-    // DrawTexture(tex, screen_width / 2, screen_height / 2, WHITE);
-    DrawTexture(tex, 0, 0, WHITE);
-
-    EndDrawing();
-    //----------------------------------------------------------------------------------
 }
